972c59d3796c3ecb02c6729f2fd338ac1561b0a8
Adrian Aiken <adaiken@outlook.com>
2021-12-06 23:23:06 -0800

    Day 7

---
+++ days/7/seven-b.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ifstream input; string line;
+    input.open("input.txt");
+
+    vector<int> crabs;
+    int maxPos = 0;
+
+    while (!input.eof()) {
+        getline(input, line, ',');
+        int crabPos = stoi(line);
+        crabs.push_back(crabPos);
+
+        if (crabPos > maxPos) maxPos = crabPos;
+    }
+
+    // Brute force it; check the fuel needed for all positions
+    int bestPos = 0, bestFuel = INT_MAX;
+
+    for (int i = 0; i < maxPos; i++) {
+        int curFuel = 0;
+        for (auto crab: crabs) {
+            int delta = abs(crab - i);
+            curFuel += (delta * (delta+1)) / 2;
+        }
+
+        if (curFuel < bestFuel) {
+            bestPos = i;
+            bestFuel = curFuel;
+        }
+    }
+
+    cout << "======================================\n"
+         << "Best Position: " << bestPos << "\n"
+         << "Fuel spent:    " << bestFuel << "\n"
+         << "======================================\n";
+}
